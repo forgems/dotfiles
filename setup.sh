8edf9f7a1778b0006348f6af87cef8d0fb0f54a8
Marcin Swiderski <marcinswiderski@opentopic.com>
2016-10-16 21:40:40 +0200

    Added tmux config

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@ DIR=$(dirname "${PWD}/$(dirname $0)")
 ln -f -s "${DIR}/.vimrc" "${HOME}/.vimrc"
 ln -f -s "${DIR}/.vim" "${HOME}/.vim"
 ln -f -s "${DIR}/.gitconfig" "${HOME}/.gitconfig"
+ln -f -s "${DIR}/.tmux.conf" "${HOME}/.tmux.conf"
